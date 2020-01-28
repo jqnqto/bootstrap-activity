ff9b4c93faa6478da0f2651b255ff46103b9f27b
Aj Quinquito <quinquito15@gmail.com>
2020-01-28 16:24:10 +0800

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+// swiper 
+// var swiper = new Swiper('.swiper-container', {
+//         pagination: '.swiper-pagination',
+//         nextButton: '.swiper-button-next',
+//         prevButton: '.swiper-button-prev',
+//         paginationClickable: true,
+//         centeredSlides: true,
+//         autoplay: 3500,
+//            speed: 1500,
+//            loop: true,
+//         autoplayDisableOnInteraction: false
+//     });
